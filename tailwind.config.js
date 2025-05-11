fe040b8549e7cc254a5615923f52a299db0afcb3
Rizky P. Mahendra <rizkymahendra2346@gmail.com>
2025-05-11 11:44:06 +0700

    project init

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+import tailwindcssAnimate from 'tailwindcss-animate'
+import typography from '@tailwindcss/typography'
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+
+    // Or if using `src` directory:
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [tailwindcssAnimate, typography],
+}
