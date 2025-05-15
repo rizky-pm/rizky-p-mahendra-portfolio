556f3b7bdbdb3271ccbaf4c6d259efdcd6586c57
Rizky P. Mahendra <rizkymahendra2346@gmail.com>
2025-05-15 15:15:22 +0700

    feat: update TechStackIcon component styles for improved responsiveness

--- src/app/(frontend)/tech-stack/components/tech-stack-icon/index.tsx
+++ src/app/(frontend)/tech-stack/components/tech-stack-icon/index.tsx
@@ -12,9 +12,9 @@ const TechStackIcon = (props: IProps) => {
 
   return (
     <div
-      className={cn('rounded mx-5 md:mx-10 flex justify-center items-center', {
-        'h-20 w-20 md:h-40 md:w-40 xl:h-50 xl:w-50': isBig,
-        'h-10 w-10 md:h-20 md:w-20 xl:h-25 xl:w-25': !isBig,
+      className={cn('rounded mx-5 my-5 xl:my-0 md:mx-10 flex justify-center items-center', {
+        'h-20 w-20 md:h-32 md:w-32 xl:h-36 xl:w-36 3xl:h-44 3xl:w-44': isBig,
+        'h-16 w-16 md:h-24 md:w-24 xl:h-28 xl:w-28 3xl:h-36 3xl:w-36': !isBig,
       })}
     >
       {children}
