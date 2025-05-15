2f7c8aac8c3a8c644e7f16dcdef0bd8d61050109
Rizky P. Mahendra <rizkymahendra2346@gmail.com>
2025-05-16 00:26:06 +0700

    feat: add metadata generation for experience and project pages

--- src/app/(frontend)/experience/[experienceId]/page.tsx
+++ src/app/(frontend)/experience/[experienceId]/page.tsx
@@ -3,11 +3,31 @@ import ExperienceDetailClient from './client'
 import configPromise from '@payload-config'
 import { getPayload } from 'payload'
 import { notFound } from 'next/navigation'
+import { generateMeta } from '@/lib/seo/generateMetaData'
+import _ from 'lodash'
 
 type Props = {
   params: {
     experienceId: string
   }
+}
+
+export const generateMetadata = async ({ params }: Props) => {
+  const payload = await getPayload({ config: configPromise })
+
+  const experience = await payload.findByID({
+    collection: 'experience',
+    id: params.experienceId,
+  })
+
+  if (!experience) return notFound()
+
+  return generateMeta({
+    title: experience.position + '-' + experience.companyName,
+    description:
+      _.toString(experience.description) ||
+      'A key position focused on delivering high-quality outcomes while supporting organizational objectives.',
+  })
 }
 
 const ExperienceDetail = async ({ params }: Props) => {
--- src/app/(frontend)/project/[projectId]/page.tsx
+++ src/app/(frontend)/project/[projectId]/page.tsx
@@ -3,11 +3,29 @@ import ProjectDetailClient from './client'
 import configPromise from '@payload-config'
 import { getPayload } from 'payload'
 import { notFound } from 'next/navigation'
+import { generateMeta } from '@/lib/seo/generateMetaData'
+import _ from 'lodash'
 
 type Props = {
   params: {
     projectId: string
   }
+}
+
+export const generateMetadata = async ({ params }: Props) => {
+  const payload = await getPayload({ config: configPromise })
+
+  const project = await payload.findByID({
+    collection: 'projects',
+    id: params.projectId,
+  })
+
+  if (!project) return notFound()
+
+  return generateMeta({
+    title: project.title,
+    description: _.toString(project.description) || 'Detailed view of a featured project.',
+  })
 }
 
 const ProjectDetail = async ({ params }: Props) => {
